5df450d17c4ba591ffe253481515ff923f1cdc73
unknown <kozhon.amantay@gmail.com>
2021-08-05 12:26:58 +0600

    general CRUD and header

--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useGames } from '../../contexts/GameContext';
-import GameCard from '../GameCard/GameCard';
-import { Carousel } from 'react-bootstrap';
+import React from "react";
+import { useEffect } from "react";
+import { useGames } from "../../contexts/GameContext";
+import GameCard from "../GameCard/GameCard";
+import { Carousel, Container } from "react-bootstrap";
 
 const GamesList = () => {
+  const { getGamesData, gamesData } = useGames();
 
-    const {getGamesData,gamesData} = useGames()
+  useEffect(() => {
+    getGamesData();
+  }, []);
 
-    useEffect(()=> {
-        getGamesData()
-    },[])
-
-    return (
-        <div style={{display:"flex",justifyContent:'center',}}>
+  return (
+    <Container>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <div className="container-div">
-           {gamesData && gamesData.map(game => {
-               return <GameCard game={game} />
-           })} 
+          {gamesData &&
+            gamesData.map((game) => {
+              console.log(game);
+              return <GameCard game={game} />;
+            })}
         </div>
-        </div>
-    );
+      </div>
+    </Container>
+  );
 };
 
 export default GamesList;
