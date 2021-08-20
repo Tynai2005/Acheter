583d937911a9e49ede33c3f05711bad822f2289a
Пользователь <tynai.s.95@gmail.com>
2021-08-20 22:27:20 +0600

    src

--- src/helper/consts.jsx
+++ src/helper/consts.jsx
@@ -11,10 +11,6 @@ export const ACTIONS = {
 export const JSON_API_USERS = "http://localhost:8000/users";
 export const PRODUCTS_API = "http://localhost:8000/products";
 
-export const REACT_APP_FIREBASE_KEY = "AIzaSyDepqa6V4PAIstxClX9xek2G2yM-IZwF4g"
-export const REACT_APP_FIREBASE_DOMAIN = "solo-hackathon.firebaseapp.com"
-export const REACT_APP_FIREBASE_PROJECT_ID = "solo-hackathon"
-export const REACT_APP_FIREBASE_STORAGE_BUCKET = "solo-hackathon.appspot.com"
-export const REACT_APP_FIREBASE_SENDER_ID = "887906957622"
+
   // REACT_APP_FIREBASE_KEY = "1:887906957622:web:0c89e4c8cc06f744d7db97",
   // REACT_APP_FIREBASE_KEY = "G-W3PQJ5Q5NS"
